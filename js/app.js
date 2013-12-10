cf2bc8d7d3c456d7ab790d70aaacbb61ef11ed4b
Kota Miyake <miyake.kota@gmail.com>
2013-12-10 21:32:41 +0900

    Create a Article model

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 (function() {
-  console.log("Hello Backbone!");
+  var Article = Backbone.Model.extend({
+    defaults: {
+      "title": "",
+      "body": "",
+      "posted_at": new Date()
+    }
+  });
+
+  var article = new Article();
+  article.set({title: "First Title!", body: "This is a nice article." });
+  console.log(JSON.stringify(article));
 }());
