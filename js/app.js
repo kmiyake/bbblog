f74b448ddc90907b51164ca8c2c0b3cbeb94b884
Kota Miyake <miyake.kota@gmail.com>
2013-12-10 22:07:28 +0900

    Create Articles collection

--- js/app.js
+++ js/app.js
@@ -7,7 +7,23 @@
     }
   });
 
-  var article = new Article();
-  article.set({title: "First Title!", body: "This is a nice article." });
-  console.log(JSON.stringify(article));
+  var Articles = Backbone.Collection.extend({
+    model: Article
+  });
+
+  var articles = new Articles();
+  var article1 = new Article({title: "First article", body: "This is a good article."});
+  var article2 = new Article({title: "Second article", body: "This is a good article."});
+
+  console.log("Add article1");
+  articles.add(article1);
+  console.log(JSON.stringify(articles));
+
+  console.log("Add article2");
+  articles.add(article2);
+  console.log(JSON.stringify(articles));
+
+  console.log("Remove article1");
+  articles.remove(article1);
+  console.log(JSON.stringify(articles));
 }());
