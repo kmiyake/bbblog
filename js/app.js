240e5b71cc8da07aec7181a74ccdc10ad3002b16
Kota Miyake <miyake.kota@gmail.com>
2013-12-11 21:17:14 +0900

    Create a Article View

--- js/app.js
+++ js/app.js
@@ -11,19 +11,32 @@
     model: Article
   });
 
-  var articles = new Articles();
-  var article1 = new Article({title: "First article", body: "This is a good article."});
-  var article2 = new Article({title: "Second article", body: "This is a good article."});
+  var ArticlesView = Backbone.View.extend({
+    el: "#articles",
+    initialize: function() {
+      var article1 = new Article({title: "First article", body: "This is a good article."});
+      var article2 = new Article({title: "Second article", body: "This is a good article."});
+      this.articles = new Articles([article1, article2]);
+    },
+    render: function () {
+      this.articles.each(function(article) {
+        this.addArticle(article);
+      }, this);
+      return this;
+    },
+    addArticle: function (article) {
+      this.$el.append(new ArticleView({model: article}).render().el);
+    }
+  });
 
-  console.log("Add article1");
-  articles.add(article1);
-  console.log(JSON.stringify(articles));
+  var ArticleView = Backbone.View.extend({
+    template: _.template($("#article").html()),
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
 
-  console.log("Add article2");
-  articles.add(article2);
-  console.log(JSON.stringify(articles));
-
-  console.log("Remove article1");
-  articles.remove(article1);
-  console.log(JSON.stringify(articles));
+  var articlesView = new ArticlesView();
+  articlesView.render();
 }());
